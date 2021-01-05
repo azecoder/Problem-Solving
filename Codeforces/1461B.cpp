a920b7bac59830c7b798127f6eed0e2ab31a5fa2
Tabriz Hajiyev <tabriz.haji@gmail.com>
2021-01-05 17:14:09 +0300

    cf round 690, 691 problems a, b, c

---
+++ Codeforces/1461B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define rep(i,a) for(int i=0; i<a; i++)
+#define rrep(i,a) for(int i=a-1; i>=0; i--)
+#define range(i,a,b) for(int i=a; i<b; i++)
+#define rrange(i,a,b) for(int i=a-1; i>=b; i--)
+
+#define ll long long
+#define MX 1e4+5
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef vector<vi> vvi;
+
+
+void run_case() {
+	
+	int n, m;
+	cin >> n >> m;
+	
+	vector<string> a;
+	for(int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		a.push_back(s);
+	}
+	
+	int sum = 0;
+	for(int i = 0; i < n; i++)
+		
+		
+	cout << sum << "\n";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+ 
+    int tests;
+    cin >> tests;
+ 
+    while (tests-- > 0)
+        run_case();
+}
